7cd65132e7f9172d137ae8f356ecc4b3b86de872
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2020-12-03 18:59:32 +0100

    Updated some files

---
+++ launch.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -e
+
+deno run --unstable --allow-net --allow-read ui.ts
+
--- ui.ts
+++ ui.ts
@@ -1,31 +1,32 @@
 import { serve } from 'https://deno.land/std@0.79.0/http/server.ts'
+import { expandGlobSync } from 'https://deno.land/std@0.79.0/fs/mod.ts'
+import { basename } from 'https://deno.land/std@0.79.0/path/mod.ts'
 import { render } from './mustache/mod.ts'
 
 const server = serve({ hostname: "0.0.0.0", port: 8080 });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+console.log(`HTTP webserver running. Access it at: http://localhost:8080/`);
 
-const bodyContent = Deno.readTextFile('./view/home.mustache');
+const home_template = Deno.readTextFile('./view/home.mustache');
 
 for await (const request of server) {
-	const now1 = new Date().getMilliseconds()
-	const view = {
-		title: 'Joe',
-		calc: () => {
-			return 5
-		}
-	}
-	const view2 = {
-		price: () => {
-			let all = []
-			for(let i = 0; i < 1000; ++i){
-				all.push(view)
-			}
-			return all
-		}
-	}
-
-	let rendered = render(await bodyContent, view2)
-	const now2 = new Date().getMilliseconds()
-	request.respond({ status: 200, body: `It took ${now1 - now2} ms to render` });
+	const start = new Date().getMilliseconds()
 	
+	const git_dirs = expandGlobSync('/home/daniel/Documents/*')
+	const view: {files: {name: string, clone_url: string}[]} = {
+		files: function(){
+			const list: {name: string, clone_url: string}[] = []
+			for(const file of git_dirs){
+				if(file.isDirectory){
+					list.push({ name: basename(file.path), clone_url: `git clone ssh://git@190405.xyz:22000/~/${basename(file.path)}`})
+				}
+			}
+			return list
+		}()
+	}
+	const rendered = render(await home_template, view)
+
+	const time = new Date().getMilliseconds() - start
+	console.log(`Took ${time} ms to render page!`)
+	
+	request.respond({ status: 200, body: rendered });
 }
