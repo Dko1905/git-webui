d2915832aa796a22c883325cca805b624664ab02
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2020-12-04 14:21:21 +0100

    Small changes

--- ui.js
+++ ui.js
@@ -1,11 +1,15 @@
 const http = require('http')
 const util = require('util')
 const readFile = util.promisify(require('fs').readFile)
+const readdir = util.promisify(require('fs').readdir)
 
 const { render } = require('mustache')
 
+// Constants
 const PORT = process.env.PORT || 8080
-
+const GIT_DIR = '.'
+const CLONE_SSH_PREFIX = 'git clone \"ssh://git@190405.xyz:22000/~/'
+// Semi-Constants
 const home_template = readFile('./view/home.mustache', { encoding: 'utf-8' })
 
 const home_handle = async (req, res) => {
@@ -13,11 +17,14 @@ const home_handle = async (req, res) => {
 		const start = Date.now()
 
 		const view = {
-			files: [
-				{ name: 'test.git', clone_url: `git clone ssh://git@190405.xyz:22000/~/${'test.git'}`}
-			]
+			files: []
 		}
-
+		const dir = await readdir(GIT_DIR)
+		dir.forEach((file) => {
+			view.files.push({
+				name: file, clone_ssh_url: `${CLONE_SSH_PREFIX}${file}"`
+			})
+		})
 		const rendered = render(await home_template, view)
 
 		res.writeHead(200)
@@ -34,7 +41,7 @@ const home_handle = async (req, res) => {
 }
 
 const app = http.createServer(async (req, res) => {
-	if(req.url == '/index.html' || req.url == '/'){
+	if((req.url == '/index.html' || req.url == '/' ) && req.method == 'GET'){
 		home_handle(req, res)
 	} else{
 		res.writeHead(404)
